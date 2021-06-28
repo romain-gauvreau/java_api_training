5e1864e3b3f024c44f8297ef30d20baf6c341c79
Romain GAUVREAU <romain.gauvreau@epita.fr>
2021-06-28 22:18:59 +0200

    Setup Maven

---
+++ src/main/java/navy_battle/Api.java
@@ -0,0 +1,26 @@
+package navy_battle;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+public class Api {
+    public void start(int port, String connectURL) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.setExecutor(Executors.newSingleThreadExecutor());
+        server.createContext("/ping", this::handlePing);
+        server.start();
+    }
+
+    private void handlePing(HttpExchange exchange) throws IOException {
+        String body = "OK";
+        exchange.sendResponseHeaders(200, body.length());
+        try (OutputStream os = exchange.getResponseBody()) { // (1)
+            os.write(body.getBytes());
+        }
+    }
+}
---
+++ src/main/java/navy_battle/Launcher.java
@@ -0,0 +1,23 @@
+package navy_battle;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(String.valueOf(Launcher.class));
+        try {
+            if (args.length == 0) {
+                logger.severe("You need to respect the following syntax: Launcher [port] [url]");
+                System.exit(-1);
+            }
+
+            int port = Integer.parseInt(args[0]);
+            logger.info("Application is starting, port " + port);
+            new Api().start(port, args.length > 1 ? args[1] : null);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
