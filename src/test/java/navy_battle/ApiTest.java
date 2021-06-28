e0a76853edb8e83553d098736b6083c0811054d6
Romain GAUVREAU <romain.gauvreau@epita.fr>
2021-06-28 22:33:11 +0200

    Add ping test

---
+++ src/test/java/navy_battle/ApiTest.java
@@ -0,0 +1,26 @@
+package navy_battle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiTest {
+
+    private final HttpClient client = HttpClient.newHttpClient();
+    @Test
+    public void
+    handlePingTest() throws Exception {
+        int port = 9976;
+        String[] args = new String[] { String.valueOf(port) };
+        Launcher.main(args);
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:" + port + "/ping")).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals("OK", response.body());
+    }
+}
